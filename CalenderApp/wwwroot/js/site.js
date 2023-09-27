ce84664f4a4ec23ff66bdf075464050b79a52488
aambrioso01 <40615096+aambrioso01@users.noreply.github.com>
2023-09-27 14:15:11 -0400

    first commit

---
+++ CalenderApp/wwwroot/js/site.js
@@ -0,0 +1,36 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets
+
+//
+document.addEventListener('DOMContentLoaded', function () {
+    var calendarEl = document.getElementById('calendar');
+
+    var calendar = new FullCalendar.Calendar(calendarEl, {
+        initialView: 'dayGridMonth',
+        headerToolbar: {
+            center: 'addEventButton'
+        },
+        customButtons: {
+            addEventButton: {
+                text: 'add event...',
+                click: function () {
+                    var dateStr = prompt('Enter a date in YYYY-MM-DD format');
+                    var date = new Date(dateStr + 'T00:00:00'); // will be in local time
+
+                    if (!isNaN(date.valueOf())) { // valid?
+                        calendar.addEvent({
+                            title: 'dynamic event',
+                            start: date,
+                            allDay: true
+                        });
+                        alert('Great. Now, update your database...');
+                    } else {
+                        alert('Invalid date.');
+                    }
+                }
+            }
+        }
+    });
+
+    calendar.render();
+});
